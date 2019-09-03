f2af764a1c8b0087cbdd7715dd5bc8ebbddaa09b
becc-mu <becc.mulugeta@gmail.com>
2019-09-03 11:58:55 +0100

    user can sign in

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 feature 'user visites hompage' do
   scenario 'sucessfuly' do
+    sign_in
     visit root_path
+
     expect(page).to have_css 'h1', text: 'Todos'
   end
 end
